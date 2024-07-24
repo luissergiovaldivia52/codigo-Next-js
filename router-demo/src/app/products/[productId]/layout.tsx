3815983c8c8943114a83a10be0d284c582717ba8
luissergiovaldivia52 <luis_sergio_valdivia@hotmail.com>
2024-07-24 16:51:11 -0300

    third

---
+++ router-demo/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,13 @@
+export default function ProductDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <h2>Feactures products</h2>
+      {/* Carrucel here */}
+    </>
+  );
+}
